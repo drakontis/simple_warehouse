d01683f288abe6e2638439bfed2edca2cdcc326a
Panagiotis Drakontis <p.drakontis@pamediakopes.gr>
2017-11-01 12:09:13 +0200

    Add spec for adding crate to store.

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -10,4 +10,20 @@ describe Store do
       expect(store.height).to eq 3
     end
   end
+
+  describe 'Add crate to store' do
+    let(:position) { Position.new(x: 5, y: 4) }
+    let(:slot) { Slot.new(width: 10, height: 3) }
+    let(:crate) { Crate.new(product_code: 'P', position: position, slot: slot) }
+
+    it 'should add crate to store' do
+      store = Store.new(width: 50, height: 30)
+      expect(store.crates.count).to be_zero
+
+      store.crates << crate
+
+      expect(store.crates.count).to eq 1
+      expect(store.crates.first).to eq crate
+    end
+  end
 end
