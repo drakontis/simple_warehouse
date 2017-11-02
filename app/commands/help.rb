2c049a048bcdda698f66a3090dc2bee922fa95e2
Drakontis Panagiotis <drakontis.panagiotis@gmail.com>
2017-11-03 00:28:37 +0200

    Fix typos in help command

--- app/commands/help.rb
+++ app/commands/help.rb
@@ -3,11 +3,11 @@ module Commands
     def execute(args = [])
       puts 'help             Shows this help message
 init W H         (Re)Initialises the application as a W x H warehouse, with all spaces empty.
-store X Y W H P  Stores a crate of product number P and of size W x H at position X,Y.
-locate P         Show a list of positions where product number can be found.
-remove X Y       Remove the crate at position X,Y.
-view             Show a representation of the current state of the warehouse, marking each position as filled or empty.
-exit             Exits the application.'
+store X Y W H P  Stores a crate of product type P and of size W x H at position X,Y.
+locate P         Shows a list of positions where product type can be found.
+remove X Y       Removes the crate at position X,Y.
+view             Shows a representation of the current state of the warehouse, marking each position as filled or empty.
+exit             Terminates the application.'
     end
   end
 end
--- spec/commands/help_spec.rb
+++ spec/commands/help_spec.rb
@@ -13,7 +13,7 @@ describe Commands::Help do
 
   describe '#execute' do
     it "should set false to warehouse's live attribute" do
-      expect{ subject.execute }.to output("help             Shows this help message\ninit W H         (Re)Initialises the application as a W x H warehouse, with all spaces empty.\nstore X Y W H P  Stores a crate of product number P and of size W x H at position X,Y.\nlocate P         Show a list of positions where product number can be found.\nremove X Y       Remove the crate at position X,Y.\nview             Show a representation of the current state of the warehouse, marking each position as filled or empty.\nexit             Exits the application.\n").to_stdout
+      expect{ subject.execute }.to output("help             Shows this help message\ninit W H         (Re)Initialises the application as a W x H warehouse, with all spaces empty.\nstore X Y W H P  Stores a crate of product type P and of size W x H at position X,Y.\nlocate P         Shows a list of positions where product type can be found.\nremove X Y       Removes the crate at position X,Y.\nview             Shows a representation of the current state of the warehouse, marking each position as filled or empty.\nexit             Terminates the application.\n").to_stdout
     end
   end
 end
